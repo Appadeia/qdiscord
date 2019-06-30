ee0c3bc78d12670004d8bbb2bea80e31c3274092
Carson Black <uhhadd@gmail.com>
2019-06-30 18:03:32 -0400

    Revert "no need for all of those"
    This reverts commit 22907ace8d4a99c158cf67397bf939c15c1afb34.

---
+++ cbuild/CMakeFiles/Desucordo_autogen.dir/RCCqmlInfo.cmake
@@ -0,0 +1,18 @@
+# Configurations
+set(ARCC_MULTI_CONFIG "FALSE")
+set(ARCC_VERBOSITY "")
+# Settings file
+set(ARCC_SETTINGS_FILE "/home/cdblack/git/qdiscord/cbuild/CMakeFiles/Desucordo_autogen.dir/RCCqmlSettings.txt")
+# Directories
+set(ARCC_BUILD_DIR "/home/cdblack/git/qdiscord/cbuild/Desucordo_autogen")
+set(ARCC_INCLUDE_DIR "/home/cdblack/git/qdiscord/cbuild/Desucordo_autogen/include")
+# Rcc executable
+set(ARCC_RCC_EXECUTABLE "/usr/lib64/qt5/bin/rcc")
+set(ARCC_RCC_LIST_OPTIONS "--list")
+# Rcc job
+set(ARCC_LOCK_FILE "/home/cdblack/git/qdiscord/cbuild/CMakeFiles/Desucordo_autogen.dir/RCCqml.lock")
+set(ARCC_SOURCE "/home/cdblack/git/qdiscord/qml.qrc")
+set(ARCC_OUTPUT_CHECKSUM "EWIEGA46WW")
+set(ARCC_OUTPUT_NAME "qrc_qml.cpp")
+set(ARCC_OPTIONS "-name;qml")
+set(ARCC_INPUTS "/home/cdblack/git/qdiscord/VersionRow.qml;/home/cdblack/git/qdiscord/main.qml")
